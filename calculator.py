3838be07808f00d11db250af62535e36bfb00ff0
Anthony Cotales <ton.cotales@gmail.com>
2025-02-02 18:53:02 +0800

    Create display frame

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
-import tkinter as tk
 import config
+import functools
+import tkinter as tk
+from tkinter import PhotoImage
+
 
 
 class Calculator(tk.Tk):
@@ -14,10 +17,63 @@ class Calculator(tk.Tk):
 	def __init__(self):
 		super().__init__()
 
+		# Set window title, minimum and maximum size, and icon
 		self.title(config.WINDOW_TITLE)
 		self.geometry(f'{config.WINDOW_WIDTH}x{config.WINDOW_HEIGHT}')
 		self.minsize(config.WINDOW_WIDTH, config.WINDOW_HEIGHT)
 		self.maxsize(config.WINDOW_WIDTH, config.WINDOW_HEIGHT)
+		self.iconphoto(False, PhotoImage(data=config.WINDOW_ICON))
+
+		# Configure the row and column layout (useful for when adding widgets)
+		self.rowconfigure(0, weight=1)
+		self.columnconfigure(0, weight=1)
+
+		# Set window background color
+		self.configure(bg=config.WINDOW_BACKGROUND_COLOR)
+
+		# Create a frame for the calculator widgets (e.g., buttons and display)
+		self.display_frame = DisplayFrame(self, relief='groove', bg=config.WINDOW_BACKGROUND_COLOR)
+		self.display_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+
+class DisplayFrame(tk.Frame):
+	"""
+	A frame that contains the display area for the calculator's input and result.
+
+	Created by: Anthony Cotales
+	Email: ton.cotales@gmail.com
+	Version: 1.0.0
+	"""
+
+	def __init__(self, master=None, **kwargs):
+		super().__init__(master, **kwargs)
+
+		# Configure row and column weights for grid layout
+		self.rowconfigure(tuple(range(11)), weight=1)
+		self.columnconfigure(tuple(range(4)), weight=1)
+
+		# Create an entry widget for the display
+		self.display = tk.Entry(self, relief='flat', justify='right', bd=10, font=config.DISPLAY_FONT, fg=config.DISPLAY_FOREGROUND_COLOR)
+		self.display.grid(row=0, column=0, columnspan=4, padx=2, pady=12, sticky='nsew')
+
+		# Set the initial display value to '0' and configure the entry widget to be readonly,
+		# simulating a typical calculator behavior
+		self.display.insert(0, '0')
+		self.display.configure(state='readonly')
+
+		# Create buttons dynamically from the 'buttons' list
+		for text, row, column in config.BUTTONS:
+
+			columnspan = 2 if text == '0' else None
+
+			button = tk.Button(self, text=text, relief='groove', font=config.BUTTON_FONT)
+			button.grid(row=row, column=column, rowspan=2, columnspan=columnspan, sticky='nsew')
+			button.configure(command=functools.partial(self.process_input, text))
+
+	def process_input(self, input_value):
+		"""Processes the input from both button clicks and keyboard presses."""
+		pass
+
+
 
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -1,15 +1,44 @@
 # Calculator application configuration file.
 
-WINDOW_HEIGHT = 380
+WINDOW_HEIGHT = 350
 WINDOW_WIDTH = 300
 WINDOW_TITLE = "Calculator"
 WINDOW_BACKGROUND_COLOR = "#5cbeff"
-WINDOW_DEFAULT_FONT = ("Verdana", 12, "normal")
 
 SCIENTIFIC_NOTATION = "e"
+
 DISPLAY_INITIAL_VALUE = "0"
 DISPLAY_ERROR = "Error"
+DISPLAY_FONT = ("Default", 20, "normal")
+DISPLAY_FOREGROUND_COLOR = "#393e46"
 DISPLAY_OPERATORS = "+–×÷"
+
 PYTHON_OPERATORS = "+-*/"
 
-OPERATOR_MAP = dict(zip(PYTHON_OPERATORS, DISPLAY_OPERATORS))
+OPERATOR_MAP = dict(zip(DISPLAY_OPERATORS, PYTHON_OPERATORS))
+
+BUTTON_FONT = ("Verdana", 12, "normal")
+
+# Button configuration: (text label, row position, column position)
+BUTTONS = [
+			('C', 1, 0), ('←', 1, 1), ('%', 1, 2), ('÷', 1, 3),
+			('7', 3, 0), ('8', 3, 1), ('9', 3, 2), ('×', 3, 3),
+			('4', 5, 0), ('5', 5, 1), ('6', 5, 2), ('–', 5, 3),
+			('1', 7, 0), ('2', 7, 1), ('3', 7, 2), ('+', 7, 3),
+			('0', 9, 0), ('.', 9, 2), ('=', 9, 3)
+        ]
+
+WINDOW_ICON = """\
+iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAB2AAAAdgFOey\
+YIAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAgtJREFUOI19kj1MU1\
+EYhp9zem+92oJAowjEmoiBKAZ/iIbJRk1MaCKTicaA0Q4mLi510ejm5EDipi4ODibGjbjqpoMaSU\
+wKBMtfpLVQpaW0tL33nuMAEgvcvtM55/veJ+/35Qg2dGHoyWkh3CtCSEkdKbTG54y+f/nwI4Cx\
+WZHuiC+wN+Lf1+5pzu1qQWRmqKZnbgJttQCkETjaR+D4WU9AahlWmztpSr/Ys+mq6RCiXno\
+QoHXtUy1ga3WrNCBqe4z/L8Xxr9jZ9Daf0ppypYq0QjSkp+skKBfpCVY4JPKszU7QZZXoskpkx8c\
+4SI5+YwEjl/IGdLQ2cffGJe5cv4jfNIjHBojHBjAMSTwW5f7tQcJtLd4jFIplkvOLLP0p4CpFIplCKYXj\
+KhI/FtgfamRldc0bUK06JJIpVgolbNvl++Q8GtBK83PiMaWGInblDLB75xFCzUEunz9JNHICR9lcjfZ\
+zLdqPafoYPveFW5HPHGjOeyfILhcY/fCNxd95yhWH1+8+AWDbLmawD1pOAUveS7T8Jsc62+k+\
+3IaQgt7uML3dYYQUuJU5KI+xVTUJLMukM9xKU2MAn5T0HOkAwCclys5CaRb00M4ArZQ7NZfh0\
+dO3lNaqVG2HByNv1pdrO9x7NUjQqjCdCQFCbQOUXfeZRvsTyfWPYhoGk7O/Ns9TmY6NoTWGlM\
+//+f4CrqzDeV+thpcAAAAASUVORK5CYII=\
+"""
