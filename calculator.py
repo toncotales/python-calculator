4de9c49299049bab429e4682c32191bfd0cbb514
Anthony Cotales <ton.cotales@gmail.com>
2025-02-02 19:10:04 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -57,17 +57,29 @@ class DisplayFrame(tk.Frame):
 
 		# Set the initial display value to '0' and configure the entry widget to be readonly,
 		# simulating a typical calculator behavior
-		self.display.insert(0, '0')
+		self.display.insert(0, config.DISPLAY_INITIAL_VALUE)
 		self.display.configure(state='readonly')
+
+		# Configure the entry widget to validate input on each key press
+		self.display.configure(validate='key', validatecommand=(self.register(self.validate_input), '%P'))
 
 		# Create buttons dynamically from the 'buttons' list
 		for text, row, column in config.BUTTONS:
 
 			columnspan = 2 if text == '0' else None
 
-			button = tk.Button(self, text=text, relief='groove', font=config.BUTTON_FONT)
+			button = tk.Button(self, text=text, relief='groove', cursor='hand2', font=config.BUTTON_FONT)
 			button.grid(row=row, column=column, rowspan=2, columnspan=columnspan, sticky='nsew')
 			button.configure(command=functools.partial(self.process_input, text))
+
+	def validate_input(self, new_value):
+		"""Validate input to ensure the length doesn't exceed 17 characters."""
+		return len(new_value) <= 17 
+
+	def clear_display(self):
+		"""Clears the display and resets it to the initial value '0'."""
+		self.display.delete(0, 'end')
+		self.display.insert(0, config.DISPLAY_INITIAL_VALUE)
 
 	def process_input(self, input_value):
 		"""Processes the input from both button clicks and keyboard presses."""
