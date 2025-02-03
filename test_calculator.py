517a9b02a04941aa1518ed796aba0b3fda31499e
Anthony Cotales <ton.cotales@gmail.com>
2025-02-03 17:01:51 +0800

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -15,10 +15,38 @@ class TestCalculator(unittest.TestCase):
     def test_invalid_expressions(self):
         """Test that invalid expressions return 'Error'."""
         values = ["2÷0", "Error", None, "1e"]
-
         for value in values:
             result = self.calc.display_frame.evaluate_expression(value)
             self.assertEqual(result, "Error")
+
+    def test_process_input(self):
+        """Test the input process functionality for button clicks or keyboard presses."""
+        values = ".2×.8"
+        result = "0.2×0.8"
+        for value in values:
+            self.calc.display_frame.process_input(value)
+        self.assertEqual(self.calc.display_frame.display.get(), result)
+
+    def test_invalid_inputs(self):
+        """Test the input functionality for invalid keyboard presses."""
+        values = "abcdefghijklmnopqrstuvwxyz"
+        for value in values:
+            self.calc.display_frame.process_input(value) # Invalid input
+            self.assertEqual(self.calc.display_frame.display.get(), "0")  # It should remain 0
+            
+    def test_clear_display(self):
+        """Test the clear display functionality."""
+        self.calc.display_frame.process_input("45678")  # Valid input numbers
+        self.calc.display_frame.process_input("\x1b")  # C or the ESC key press
+        self.assertEqual(self.calc.display_frame.display.get(), "0")
+
+    def test_backspace_function(self):
+        """Test that backspace works as expected."""
+        backspaces = ['←', '\x08']  # Either button click or the backspace key press
+        self.calc.display_frame.process_input("12")
+        for backspace in backspaces:
+            self.calc.display_frame.process_input(backspace)
+        self.assertEqual(self.calc.display_frame.display.get(), "0")
 
 
 
